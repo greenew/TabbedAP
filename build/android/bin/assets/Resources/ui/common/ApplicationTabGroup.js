f3087e60395c1ce3cdb4fdb3400f6fdcd7668554
greenew <greenew@moscow.com>
2015-02-20 20:50:35 -0800

    Personalized

--- build/android/bin/assets/Resources/ui/common/ApplicationTabGroup.js
+++ build/android/bin/assets/Resources/ui/common/ApplicationTabGroup.js
@@ -3,9 +3,45 @@ function ApplicationTabGroup(Window) {
 	var self = Ti.UI.createTabGroup();
 
 	//create app tabs
-	var win1 = new Window(L('home')),
-		win2 = new Window(L('settings'));
+	var win1 = Ti.UI.createWindow({
+		title:L('home'),
+		backgroundColor: '#006633',
+		layout: 'vertical'
+	}),
+		win2 = Ti.UI.createWindow({
+		//title:L(''),
+		backgroundColor: '#33B366',
+		layout: 'horizontal',
+	});
 
+	var Textbox1 = Ti.UI.createLabel({
+  		color: '#900',
+  		font: { fontSize:48 },
+  		shadowColor: '#aaa',
+  		shadowOffset: {x:5, y:5},
+  		shadowRadius: 3,
+  		text: 'Learn Javascript with ease!',
+  		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+  		top: 30,
+  		width: Ti.UI.SIZE, height: Ti.UI.SIZE
+	});
+	win1.add(Textbox1);
+	
+	var Textbox2 = Ti.UI.createLabel({
+  		color: '#800',
+  		font: { fontSize:48 },
+  		shadowColor: '#aaa',
+  		shadowOffset: {x:5, y:5},
+  		shadowRadius: 3,
+  		text: 'Dream on...',
+  		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+  		top: 30,
+  		width: Ti.UI.SIZE, height: Ti.UI.SIZE
+	});
+	win2.add(Textbox2);
+	
+	
+	
 	var tab1 = Ti.UI.createTab({
 		title: L('home'),
 		icon: '/images/KS_nav_ui.png',
