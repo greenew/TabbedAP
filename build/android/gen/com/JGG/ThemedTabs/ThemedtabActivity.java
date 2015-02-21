33dd4cb74fdc19092510632246cdac54722603a9
greenew <greenew@moscow.com>
2015-02-20 19:00:14 -0800

    Push this to online repo

---
+++ build/android/gen/com/JGG/ThemedTabs/ThemedtabActivity.java
@@ -0,0 +1,7 @@
+package com.JGG.ThemedTabs;
+
+import org.appcelerator.titanium.TiRootActivity;
+
+public final class ThemedtabActivity extends TiRootActivity
+{
+}
---
+++ build/android/gen/com/JGG/ThemedTabs/ThemedtabAppInfo.java
@@ -0,0 +1,70 @@
+package com.JGG.ThemedTabs;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class ThemedtabAppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public ThemedtabAppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "com.JGG.ThemedTabs";
+	}
+
+	public String getName() {
+		return "ThemedTab";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "green_000";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by green_000";
+	}
+
+	public String getDescription() {
+		return "";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "72f38b0f-ace8-4c9b-8da2-08b0380fc550";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
